a8eaf136848797b5a5ac6ae3ad094bab11da8fb9
Veera Raita <fvera003@edu.xamk.fi>
2026-02-03 11:44:29 +0200

    Dropped in ink and base for event system

---
+++ Assets/Events/TestEvent.ink
@@ -0,0 +1,9 @@
+You find yourself in a cabin.
+
+
+
+ * There were two choices.
+ * There were four lines of content.
+
+- They lived happily ever after.
+    -> END
---
+++ Assets/Scripts/EventSystem/EventFunctions.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+/// <summary>
+/// ALL FUNCTIONS IN THIS CLASS MUST BE NAMED EXACTLY THE SAME AS THE INK TAG
+/// THAT IS USED TO CALL THEM, OTHERWISE EVERYTHING BREAKS AND I EXPLODE.
+/// </summary>
+public class EventFunctions : MonoBehaviour
+{
+    private EventVariables eventVariables => EventManager.Instance.eventVariables;
+    private EventManager eventManager => EventManager.Instance;
+
+    public void TestFunction()
+    {
+        Debug.Log("Test function was called!");
+        Debug.Log($"State of test variable: {eventManager.GetVariableState("g_test_seen")}");
+    }
+}
